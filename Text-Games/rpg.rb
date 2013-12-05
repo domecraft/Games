b134b82de40c42879a581dfc3fcc1c9bb3344c18
Christopher Durr <christopherdurr1@gmail.com>
2013-12-05 11:12:06 -0800

    Update rpg.rb

--- Text-Games/rpg.rb
+++ Text-Games/rpg.rb
@@ -2,7 +2,7 @@
 #Please contribute to the code if you want to, as it still needs work
 health = 100
 inventory = []
-puts "Welcome to this RPG. It is still in development, but hopefully you like it."
+puts "Welcome to this RPG. It is still in development but hopefully you still enjoy it."
 until playerClassChosen = true do
 	puts "First, pick a class. Do you want to be an archer, a warrior, or a wizard?"
 	playerClass = gets.chomp
