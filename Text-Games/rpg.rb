b7aae397e806092fd7e39342f8b6c7d68e9b7297
Christopher Durr <christopherdurr1@gmail.com>
2013-12-05 11:21:10 -0800

    Updated rpg.rb added printing stats
    Added to the code where it puts the stats that your class has. Also made it so it loops when you do not input an actual class.

--- Text-Games/rpg.rb
+++ Text-Games/rpg.rb
@@ -2,28 +2,38 @@
 #Please contribute to the code if you want to, as it still needs work
 health = 100
 inventory = []
+playerClassChosen = false
 puts "Welcome to this RPG. It is still in development but hopefully you still enjoy it."
-until playerClassChosen = true do
-	puts "First, pick a class. Do you want to be an archer, a warrior, or a wizard?"
-	playerClass = gets.chomp
+    while playerClassChosen == false do 
+    puts "First, pick a class. Do you want to be an archer, a warrior, or a wizard?"
+    playerClass = gets.chomp
 	playerClass = playerClass.downcase
 	if playerClass == "archer"
 		strength = 4
 		speed = 7
 		reputation = 8
 		playerClassChosen = true
+        puts "Your Strength is: #{strength}"
+        puts "Your Speed is: #{speed}"
+        puts "Your reputation is: #{reputation}"
 	elsif playerClass == "warrior"
 		strength = 8
 		speed = 4
 		reputation = 6
 		playerClassChosen = true
+        puts "Your Strength is: #{strength}"
+        puts "Your Speed is: #{speed}"
+        puts "Your reputation is: #{reputation}"
 	elsif playerClass == "wizard"
 		strength = 7
 		speed = 6
 		reputation = 4
 		playerClassChosen = true
+        puts "Your Strength is: #{strength}"
+        puts "Your Speed is: #{speed}"
+        puts "Your reputation is: #{reputation}"
 	else 
 		puts "That's not an actual class!"
 		end
-end
+    end 
 	
