92065af1622124fd0752294a6bba335ce0a66650
Christopher Durr <christopherdurr1@gmail.com>
2013-12-06 19:07:32 -0800

    Created an error message and added a race choice
    Created the function errorMessage that gets called whenever the user does not enter one of the options that are listed. Also added races that the user can pick that will modify their current stats.
    
    Also working on the choice that the user makes such as going into a tavern or into a forest.

--- Text-Games/rpg.rb
+++ Text-Games/rpg.rb
@@ -4,40 +4,79 @@ health = 100
 inventory = []
 playerClassChosen = false
 money = 0
+def errorMessage()
+	puts "You either didn't put one of the options or spelled it wrong"
+end
 puts "Welcome to this RPG. It is still in development but hopefully you still enjoy it."
     while playerClassChosen == false do 
     puts "First, pick a class. Do you want to be an archer, a warrior, or a wizard?"
     playerClass = gets.chomp
-	playerClass = playerClass.downcase
+    playerClass = playerClass.downcase
     case playerClass
 	when "archer"
 		strength = 4
 		speed = 8
 		reputation = 8
 		playerClassChosen = true
-        puts "Your Strength is: #{strength}"
-        puts "Your Speed is: #{speed}"
-        puts "Your reputation is: #{reputation}"
+        	puts "Your Strength is: #{strength}"
+        	puts "Your Speed is: #{speed}"
+        	puts "Your reputation is: #{reputation}"
 	when "warrior"
 		strength = 8
 		speed = 3
 		reputation = 6
 		playerClassChosen = true
-        puts "Your Strength is: #{strength}"
-        puts "Your Speed is: #{speed}"
-        puts "Your reputation is: #{reputation}"
+		puts "Your Strength is: #{strength}"
+        	puts "Your Speed is: #{speed}"
+        	puts "Your reputation is: #{reputation}"
 	when "wizard"
 		strength = 7
 		speed = 5
 		reputation = 4
 		playerClassChosen = true
-        puts "Your Strength is: #{strength}"
-        puts "Your Speed is: #{speed}"
-        puts "Your reputation is: #{reputation}"
-	else 
-	puts "That's not an actual class!"
-		end    
-    end 
+        	puts "Your Strength is: #{strength}"
+        	puts "Your Speed is: #{speed}"
+        	puts "Your reputation is: #{reputation}"
+	else
+		errorMessage()
+	end    
+end 
+playerRaceChosen == false
+while playerRaceChosen == false do
+	puts "Next, let's pick a race for your character"
+	puts "Do you want to be a human, an elf, or a dwarf?"
+	playerRace = gets.chomp
+	playerRace = playerRace.downcase
+	case playerRace
+		when "human"
+			strength += 3
+			speed += 1
+			reputation += 2
+			playerRaceChosen == true
+			puts "Your new Strength is: #{strength}"
+        		puts "Your new Speed is: #{speed}"
+        		puts "Your new reputation is: #{reputation}"
+		when "elf"
+			strength -=1
+			speed += 3
+			reputation -=2
+			playerRaceChosen == true
+			puts "Your new Strength is: #{strength}"
+        		puts "Your new Speed is: #{speed}"
+        		puts "Your new reputation is: #{reputation}"
+		when "dwarf"
+			strength =+5
+			speed -= 4
+			# Note -- reputation stays the same for dwarf. People in this RPG view them neutrally
+			playerRaceChosen == true
+			puts "Your new Strength is: #{strength}"
+        		puts "Your new Speed is: #{speed}"
+        		puts "Your new reputation is: #{reputation}"
+		else
+			errorMessage()
+	end
+			
+end
 puts "You start out in the middle of a town."
 puts " You overhear a person calling for help. Do you wish to help them? Y/N"
 randEvent1 = gets.chomp
@@ -54,10 +93,26 @@ case randEvent1
 			reputation -= 2
 	when "n" || "no"
 		puts "Ok then."
+locationChoiceChosen == false
+while locationChoiceChosen == false do
 puts "Do you want to go to a Tavern next?"
 puts "Or maybe you want to go to the forest?"
-locationChoice=gets.chomp
+locationChoice = gets.chomp
+locationChoice = locationChoice.downcase
 case locationChoice
-	 	
+	when "tavern"
+		puts "You are now in the tavern. You can either:"
+		puts "1: Talk to the bartender"
+		puts "2: Play a game of poker"
+		puts "3: Walk out out of the tavern"
+		tavernChoice = gets.chomp
+		tavernchoice = tavernChoice.downcase
+	when "forest"
+		puts "You find yourself in a forest. You see a deer in front of you."
+		puts "Do you wish to attack the Deer?"
+		
+	else
+		puts "That is not an option!"
+end
 	
 	
