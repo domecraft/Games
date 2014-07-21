51d3e2713206cd6a0719f81d89bab358ab9abec0
Christopher Durr <christopherdurr1@gmail.com>
2014-07-20 18:46:02 -0700

    added races and various other items

--- RPG/Engine.py
+++ RPG/Engine.py
@@ -1,11 +1,14 @@
 class character:
-    def __init__(self, name, health, race, status, strength, defense ):
+    def __init__(self, name, health, race, role, status, strength, defense, magic, bounty):
         self.name = name
         self.health = health
         self.status = status
         self.strength = strength
         self.defense = defense
         self.race = race
+        self.role = role
+        self.bounty = bounty
+        self.magic = magic
         inventory = []
         
     def modify_health(self, amount):
@@ -35,6 +38,17 @@ class character:
     def set_race(self, race):
         self.race = race
 
+    def modify_bounty(self, amount):
+        self.bounty += amount
+        
+    def checkDead(self, health):
+        if self.health <= 0:
+            self.status = "dead"
+            return "dead"
+        else:
+            self.status = "alive"
+            return "alive"
+            
 class store:
     def __init__(self, name  = "General Store" , owner = "Store Owner", alliance = "Rebellion"):
         self.name = name
@@ -44,8 +58,71 @@ class store:
             'longsword': {'cost': 10, 'speed': 3, 'strength': 7, 'defense': 2},
             'shortsword': {'cost': 8, 'speed': 5, 'strength': 4, 'defense': 2},
             'bronze_armor': {'cost': 10, 'speed': -2, 'strength': 1, 'defense': 6},
-            'silver_armor': {'cost': 20, 'speed': -5, 'strength': 2, 'defense': 12}
+            'silver_armor': {'cost': 20, 'speed': -5, 'strength': 2, 'defense': 12},
+            'platinum_armor': {'cost': 35, 'speed': -8, 'strength': 4, 'defense': 20}
             }
        
+class town:
+    def __init__(self, name, ruler, alliance, income, population):
+        self.name = name
+        self.ruler = ruler
+        self.alliance = alliance
+        self.income = income
+        self.population = population
+    def set_ruler(self, ruler):
+        self.ruler = ruler
+    
+    def set_name(self, name):
+        self.name = name
+        
+    def set_alliance(self, alliance):
+        self.alliance = alliance
+        
+    def modify_income(self, income):
+        self.income += income
+    
+    def modify_pop(self, population):
+        self.population += population
+    
+def introduction():
+    print "Welcome to the game Asgarth!\nYou are immigrating from a foreign land in hopes of making:\n\n1.Fame\n2.Fortune\n3.Power"
+    print "First, however, I would like to ask you a few questions"
+    
+#Character creation
+characterCreationFinished = False
+while characterCreationFinished == False:
+    print ""
+    name = raw_input("What is your name, adventurer?: ")
+    print""
+    race = str(raw_input ("What shall your race be?:\n1.Human\n2.Elf\n3.Dwarf\n"))
+    race = race.lower()
+    if (race == "1") or (race == "human"):
+        strength = 6
+        defense = 6
+        magic = 3
+    elif (race == "2") or (race == "elf"):
+        strength = 3
+        defense = 7
+        magic = 5
+    elif (race == "3") or (race == "dwarf"):
+        strength = 7
+        defense = 3
+        magic = 5
+    elif (race == "saiyan"):
+        strength = 9001
+        defense = 9001
+    else:
+        print "That is not a valid race in this game!"
+    
+    role = str(raw_input"What did you study before you arrived?\n1.Magic\n2.Warfare\n3.History\n" ))
+    if (role == "1") or (role == "magic"):
+        magic += 3
+    if (role == "2") or (role == "warfare"):
+        strength += 3
+    if (role == "3") or (role == "history"):
+        defense +=3
+        
+
+    
             
         
