91bf033cc7abb42ce60fbb310c1d385546a2debd
Christopher Durr <christopherdurr1@gmail.com>
2014-08-11 21:00:27 -0700

    changes

--- WorldHistory.rb
+++ WorldHistory.rb
@@ -4,34 +4,34 @@
 # Enjoy the game!
 
 def error
-  puts ""    
+  puts   
   puts "I'm sorry, that is not a valid option"
-  puts ""
+  puts 
 end
 
 def space
-  puts " "
+  puts 
 end
 
 puts "Welcome to World War II!"
-puts "To begin, you must first pick a leader to play as."
+puts "To begin, you must first pick a leader to play as..."
+space
 leaderChosen = false
 while leaderChosen == false do
   puts "Here are your options:"
-  space()
+  space
   puts "1. Adolf Hitler - Germany"
   puts "2. Franklin Roosevelt - USA"
   puts "3. Benito Mussolini - Italy"
   puts "4. Tojo Hideki - Japan"
   puts "5. Joseph Stalin - USSR (Soviet Union)"
-  puts "6. Hirohito - Japan"
-  space()
+  space
   print "Pick the number of the leader: "
   chooseLeader = gets.chomp.downcase
     case chooseLeader 
         when "1", "2", "3", "4", "5", "6"
         leaderChosen = true
-        else error()
+        else error
     end
 end
 charisma = 5
@@ -46,14 +46,18 @@ case chooseLeader
         army += 1
         navy -= 3
         info = "- As Adolf Hitler, you were the leader of the Nazi party in Germany.\n- You also caused the Holocaust, one of the largest genocides in history.\n- Your beliefs were written in a book called 'Mein Kampf'"
+        role = "dictator"
+        alliance = "axis"
     when "2"
         leaderName = "Franklin Roosevelt"
-        country = "United States of America"
+        country = "The United States of America"
         charisma += 3
         army += 4
         navy += 3
         territories += 4
         info = "- As Franklin Roosevelt, you helped the United States through uncertain times.\n- Your fireside chats made you a popular figure with American citizens.\n- You also had polio, a fact you tried to hide from others."
+        role = "president"
+        alliance = "allies"
     when "3"
         leaderName = "Benito Mussolini"     
         country = "Italy"
@@ -62,6 +66,8 @@ case chooseLeader
         navy -= 1
         territories -= 1
         info = "- As Benito Mussolini, you led Italy to fascism.\n- Originally you were a member of the Italian Socialist Party."
+        role = "leader"
+        alliance = "axis"
     when "4" 
       leaderName = "Tojo Hideki"
       country = "Japan"
@@ -69,6 +75,9 @@ case chooseLeader
       army += 1
       navy += 4
       territories -= 2
+      info = "- As Prime Minister of Japan, you were directly responsible for the attack on Pearl Harbor.\n- Near the end of the war, you were setenced to death for Japanese war crimes.\n- You also forced militarism and nationalism indoctrination in schools.\n- Your popularity was very high during the early years of the war."
+      role = "prime minister"
+      alliance = "axis"
     when "5"
       leaderName = "Joseph Stalin"
       country = "USSR"
@@ -76,18 +85,43 @@ case chooseLeader
       army += 2
       navy += 1
       territories += 1
-    when "6"
-      leaderName = "Hirohito"
-      country = "Japan"
-      charisma += 1
-      army += 4
-      navy +=1
-      territories -= 3
+      info = "- As Joseph Stalin, you were the leader of the Soviet Union.\n- You installed communism into the Soviet Union and created a command economy.\n- You entered into a non-agression pact with Germany, which they violated.\n- You led the Soviet Union through its post-war reconstruction phase."
+      role = "leader"
+      alliance = "allies"
 end
-space()
+space
 puts "Your name is #{leaderName} and you are the leader of #{country}"
-space()
+space
 puts info
+space
+puts "Here are the current attributes that you have right now:\nCharisma: #{charisma}\nArmy: #{army}\nNavy: #{navy}\nTerritories: #{territories}"
+space
+puts "As the #{role} of #{country}, your job is to ensure the wellbeing of your country."
+space
+puts "This can be done in a variety of ways:\n1. By attacking other countries\n2. By creating peace treaties with other countries\n3. Increasing your charisma so your citizens adore you."
+space
+puts "You initially believe that for #{country}, your best option would be to attack a smaller country and gain more citizens."
+space 
+case alliance
+  when "axis"
+    puts "As part of the axis powers, you decide that you will invade a small territory owned by The United States"
+    puts "There are two territories: The Midway Islands and Puerto Rico."
+    space
+    attackChosen = false
+    while attackChosen == false do
+      puts "Which territory do you want to attack?"
+      attackTerritory = gets.chomp.downcase
+      if attackTerritory.include? "midway" or "island"  
+        attackChosen = true
+      elsif attackTerrity.include? "puerto" or "rico"
+        attackChosen = true
+      else
+        error
+    end
+  when "allies"
+  else error
+end
+
 
 
 
