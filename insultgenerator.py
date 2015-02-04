27dfe0be5dcc2e1b83ce861ca2955787555736cd
Christopher Durr <christopherdurr1@gmail.com>
2015-02-04 11:07:41 -0800

    Update insultgenerator.py

--- insultgenerator.py
+++ insultgenerator.py
@@ -1,11 +1,10 @@
 import random
 subject = ["Thou", "mother", "father", "face", "eyes", "hair", "lips", "half-removed brother", "sword", "hands", "bosom", "feet"]
 superlative = ["sillier", "poorer", "fatter", "uglier", "less skilled", "more frightening", "more jubilant", "more heartless", "more cowardly", "stranger", "more amusing", "less interesting"]
-adjective = ["witty", "crazy", "impeccable", "idiotic", "angsty", "arrogant", "fascinating", "slightly arousing", "charming", "surprisingly helpful", "incestuous", "curious", "weaboo", "scandalous", "witty", "comical", "adorable", "perverse", "sweet", "preposterous"]
-noun = ["swine", "Romeo and Juliet", "peacocks", "feuding families" ,"star-crossed lovers", "Hamlet", "kings of England", "Hamlet's mother", "pack of ruffians", "gang of peddlers", "kings and queens", "merrymen", "town crier", "harem of women"]
-stuff = ["in", "under", "near", "close to", "within", "around", "underneath", "outside", "inside", "betwixt", "from", "coming from", "entering", "escaping", "avoiding", "leaving", "pondering", "contemplating", "examining"]
+adjective = ["witty", "crazy", "impeccable", "violent", "idiotic", "angsty", "arrogant", "baby-faced", "fascinating", "slightly arousing", "charming", "surprisingly helpful", "incestuous", "curious", "weaboo", "scandalous", "extravagant", "terrifying", "witty", "comical", "adorable", "perverse", "sweet", "preposterous"]
+noun = ["swine", "Romeo and Juliet", "peacocks", "feuding families", "cherubs", "star-crossed lovers", "Hamlet", "kings of England", "Hamlet's mother", "pack of ruffians", "gang of peddlers", "kings and queens", "Dung beetle","merrymen", "town crier", "harem of women"]
+stuff = ["in", "under", "near", "close to", "within", "around",  "underneath", "outside", "inside", "betwixt", "from", "coming from", "entering", "escaping", "avoiding", "leaving", "pondering", "contemplating", "examining"]
 places = ["the moonlight", "the stars", "the sea", "a castle", "a reflection", "a jail cell", "some scaffolding", "the town square", "a church", "the catacombs", "clouds", "a cellar", "a cemetery", "a mirror", "a bridge"]
-
 subjectX = subject[random.randint(0, len(subject) - 1)]
 superlativeX = superlative[random.randint(0, len(superlative) - 1)]
 adjectiveX = adjective[random.randint(0, len(adjective) - 1)]
@@ -13,11 +12,11 @@ nounX = noun[random.randint(0, len(noun) - 1)]
 stuffX = stuff[random.randint(0, len(stuff) - 1)]
 placesX = places[random.randint(0, len(places) - 1)]
 if (subjectX == "Thou"):
-	phrase = subjectX + " art " + superlativeX + " than " + adjectiveX + " " + nounX + " " + stuffX + " " + placesX + "\n"
+    phrase = subjectX + " art " + superlativeX + " than " + adjectiveX + " " + nounX + " " + stuffX + " " + placesX + "\n"
 else:
-	phrase = "Thine " + subjectX + " art " + superlativeX + " than " + adjectiveX + " " + nounX + " " + stuffX + " " + placesX + "\n"
+    phrase = "Thine " + subjectX + " art " + superlativeX + " than " + adjectiveX + " " + nounX + " " + stuffX + " " + placesX + "\n"
+    print phrase
 
-print phrase
 print("Would you like to save this phrase?")
 savePhrase = raw_input("")
 if ((savePhrase == "yes") or (savePhrase == "y")):
